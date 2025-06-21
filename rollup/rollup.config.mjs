d92b3b5fe9c4e1b66f9b67cc114a829b24555926
smetankin denis nikolaevich <smetankin.denis@yandex.ru>
2025-06-21 22:29:28 +0300

    rollup2

--- rollup/rollup.config.mjs
+++ rollup/rollup.config.mjs
@@ -1,5 +1,9 @@
 import terser from "@rollup/plugin-terser";
 import css from "rollup-plugin-css-only";
+import image from "@rollup/plugin-image";
+import serve from "rollup-plugin-serve";
+import { babel } from "@rollup/plugin-babel";
+import livereload from "rollup-plugin-livereload";
 export default {
 	input: "src/main.js",
 	output: {
@@ -7,6 +11,12 @@ export default {
 		format: "cjs",
 	},
 	plugins: [
+		livereload({
+			watch: "dist",
+		}),
+		babel({ babelHelpers: "bundled" }),
+		serve({ contentBase: "dist" }),
+		image(),
 		terser(),
 		css({
 			output: "assets/bundle.css",
--- rollup/src/main.js
+++ rollup/src/main.js
@@ -1,16 +1,31 @@
 import "./index.css";
+import freebsd from "./images/freebsd.gif";
+import freebsd2 from "./images/freebsd2.png";
+import freebsd3 from "./images/freebsd3.jpg";
 const main = document.createElement("div");
 main.classList = "container";
 const body = document.querySelector("body");
 
 if (document.URL.includes("index.html")) {
 	body.classList = "index";
+	const img = document.createElement("img");
+	img.src = freebsd;
+	img.style = "width:200px";
+	body.prepend(img);
 }
 if (document.URL.includes("about.html")) {
 	body.classList = "about";
+	const img = document.createElement("img");
+	img.src = freebsd2;
+	img.style = "width:200px";
+	body.prepend(img);
 }
 if (document.URL.includes("descriptions.html")) {
 	body.classList = "descriptions";
+	const img = document.createElement("img");
+	img.src = freebsd3;
+	img.style = "width:200px";
+	body.prepend(img);
 }
 const ArrLinks = ["index", "about", "descriptions"];
 function createElements(mainElements, arrLinks) {
